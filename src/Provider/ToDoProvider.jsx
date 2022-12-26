7771a069593cd42befa390619165743479879716
mythily-muthu <mythilimuthu18@gmail.com>
2022-12-26 18:51:36 +0530

    Feat: Added the todoprovider component

---
+++ src/Provider/ToDoProvider.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+export let ToDoContext = createContext();
+
+let initialTodoListState = ["read the book"];
+
+const ToDoProvider = ({ children }) => {
+  let [todoList, setTodoList] = useState(initialTodoListState);
+
+  let contextValue = {
+    todoList,
+  };
+
+  return <ToDoContext.Provider value={{}}>{children}</ToDoContext.Provider>;
+};
+
+export let useTodoContext = () => useContext(ToDoContext);
+
+export default ToDoProvider;
